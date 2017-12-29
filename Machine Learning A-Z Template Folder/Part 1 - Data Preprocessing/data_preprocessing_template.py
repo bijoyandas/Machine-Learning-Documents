e5a08f5d2a34e6204bed3ff1dc4b7282f52e8cd0
Bijoyan Das <bthesource@gmail.com>
2017-12-29 20:22:56 +0530

    Adding all the files first time

---
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -0,0 +1,20 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv("Data.csv")
+print("Hello")
+
+# Splitting the dataset into the Training set and Test set
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
